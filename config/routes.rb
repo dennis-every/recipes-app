21082dac25523f132cbdbf58cadbdb0c1285d05d
Dennis Every <dennis@ansyra.com>
2023-05-23 17:38:08 -0400

    Add routes, controller, views for Shopping List

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'home#index'
+  get '/general_shopping_list', to: 'shopping_list#index', as: 'general_shopping_list'
 end
