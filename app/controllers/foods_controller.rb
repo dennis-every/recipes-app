afdbb048838ca7445cf235cdaa4914eff619e449
Dennis Every <dennis@ansyra.com>
2023-05-23 12:15:53 -0400

    Add Food #new, #create

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,10 +3,29 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @food = Food.find(params[:id])
 
     @food.destroy
     redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
