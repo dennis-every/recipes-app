1a524878ab8a839936a1f5cf2c350dc52c0ecdde
ashmalzahra <ashmal.zahraa@gmail.com>
2023-05-25 00:40:08 +0500

    Fix errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,31 +1,31 @@
 class RecipesController < ApplicationController
-    def index
-        @recipes = Recipe.all
+  def index
+    @recipes = Recipe.all
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @recipe = Recipe.new(recipe_params)
-        if @recipe.save
-        redirect_to recipes_path
-        else
-        render :new, status: :unprocessable_entity
-        end
-    end
+  def new
+    @recipe = Recipe.new
+  end
 
-    def new
-        @recipe = Recipe.new
-    end
+  def destroy
+    @recipe = Recipe.find(params[:id])
 
-    def destroy
-        @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
 
-        @recipe.destroy
-        redirect_to recipes_path
-    end
+  private
 
-    private
-
-    def recipe_params
-        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
