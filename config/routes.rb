e3372a58a6c7fbb7fd7c9f6a7634651d5a1a47d2
Dennis Every <dennis@ansyra.com>
2023-05-26 10:54:54 -0400

    Remove home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, except: [:edit, :update] do
     resources :recipe_foods, only: [:new, :create, :destroy]
   end
-  root 'home#index'
   get '/general_shopping_list', to: 'shopping_list#index', as: 'general_shopping_list'
   get '/public_recipes', to: 'recipes#public'
+  root 'recipes#public'
 end
